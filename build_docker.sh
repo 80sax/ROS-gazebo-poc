80sax/assignment

Doc and Refactor

build_docker.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-IMAGE_TAG=asotelo/steerai/polaris:0.0.1
+# This script is intended for local development and testing
+# It builds and runs the simulator Docker image
+# In CI the image is built by Kaniko and pushed to Docker Hub
+# The simulator is run in GUI mode by default
+# build_socker.sh --headless will run the container in headless mode
+
+IMAGE_TAG=soteloa/assignment:latest
+CONTAINER=steerai
 DISPLAY=192.168.100.224:0.0
 
 docker build -t ${IMAGE_TAG} .
-
-STOPPED_CONTAINERS=$(docker ps -aq)
-if [ -n "$STOPPED_CONTAINERS" ]; then
-  docker rm $STOPPED_CONTAINERS
-fi
-
+docker rm ${CONTAINER} -f
 if [ "$1" == "--headless" ]; then
-  docker run -it --name steerai ${IMAGE_TAG} "/bin/bash"
+  docker run -it --name ${CONTAINER} ${IMAGE_TAG} "/bin/bash"
 else
-  docker run -it -e DISPLAY=${DISPLAY} --name steerai ${IMAGE_TAG} "/bin/bash"
+  docker run -it -e DISPLAY=${DISPLAY} --name ${CONTAINER} ${IMAGE_TAG} "/bin/bash"
 fi

80sax/ROS-gazebo-poc

build_docker.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-IMAGE_TAG=asotelo/steerai/polaris:0.0.1
+# This script is intended for local development and testing
+# It builds and runs the simulator Docker image
+# In CI the image is built by Kaniko and pushed to Docker Hub
+# The simulator is run in GUI mode by default
+# build_socker.sh --headless will run the container in headless mode
+
+IMAGE_TAG=soteloa/assignment:latest
+CONTAINER=ros
 DISPLAY=192.168.100.224:0.0
 
 docker build -t ${IMAGE_TAG} .
-
-STOPPED_CONTAINERS=$(docker ps -aq)
-if [ -n "$STOPPED_CONTAINERS" ]; then
-  docker rm $STOPPED_CONTAINERS
-fi
-
+docker rm ${CONTAINER} -f
 if [ "$1" == "--headless" ]; then
-  docker run -it --name steerai ${IMAGE_TAG} "/bin/bash"
+  docker run -it --name ${CONTAINER} ${IMAGE_TAG} "/bin/bash"
 else
-  docker run -it -e DISPLAY=${DISPLAY} --name steerai ${IMAGE_TAG} "/bin/bash"
+  docker run -it -e DISPLAY=${DISPLAY} --name ${CONTAINER} ${IMAGE_TAG} "/bin/bash"
 fi